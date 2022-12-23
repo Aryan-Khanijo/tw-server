35ff38914d2fdfb4a44e939e7f0b87f51b3798e3
Aryan-Khanijo <akhanijo12345@gmail.com>
2022-12-23 23:36:51 +0530

    tweets table

---
+++ server/schema/migrations/20221223175447_tweets.js
@@ -0,0 +1,21 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+	return knex.schema.createTableIfNotExists('tweets', (table) => {
+		table.increments('id').primary();
+		table.string('content', 280).notNullable();
+		table.integer('user_id').references('id').inTable('users');
+		table.dateTime('created_at').notNullable().defaultTo(knex.fn.now());
+		table.dateTime('updated_at').notNullable().defaultTo(knex.fn.now());
+	})
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+	return knex.schema.dropSchemaIfExists('tweets');
+};
