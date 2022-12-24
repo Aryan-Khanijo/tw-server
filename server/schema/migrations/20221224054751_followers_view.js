78f9281632aa2f358ec6e6aa5761b9d786a55083
Aryan-Khanijo <akhanijo12345@gmail.com>
2022-12-24 14:13:12 +0530

    db view down fix

--- server/schema/migrations/20221224054751_followers_view.js
+++ server/schema/migrations/20221224054751_followers_view.js
@@ -3,7 +3,7 @@
  * @returns { Promise<void> }
  */
 exports.up = function (knex) {
-	return knex.schema.createView('followers_view', (view) => {
+	return knex.schema.createViewOrReplace('followers_view', (view) => {
 		view.columns(['user_id', 'followers']);
 		view.as(knex.raw('select followed_id, array_agg(follower_id) followers from relations group by followed_id;'))
 	})
--- server/schema/migrations/20221224073922_basic_user_info_view.js
+++ server/schema/migrations/20221224073922_basic_user_info_view.js
@@ -3,9 +3,9 @@
  * @returns { Promise<void> }
  */
 exports.up = function (knex) {
-	return knex.schema.createView('user_info_view', (view) => {
+	return knex.schema.createViewOrReplace('user_info_view', (view) => {
 		view.columns(['user_id', 'username', 'followers', 'following']);
-		view.as(knex.raw('select users.id, users.username, count(followers_view.followers), count(following_view.following) from users left join followers_view on users.id = followers_view.user_id left join following_view on users.id = following_view.user_id group by users.id;'))
+		view.as(knex.raw('select users.id, users.username, count(followers_view.followers), count(following_view.following), users.slug from users left join followers_view on users.id = followers_view.user_id left join following_view on users.id = following_view.user_id group by users.id;'))
 	})
 };
 
@@ -14,5 +14,5 @@ exports.up = function (knex) {
  * @returns { Promise<void> }
  */
 exports.down = function (knex) {
-
+	return knex.schema.dropViewIfExists('user_info_view');
 };
