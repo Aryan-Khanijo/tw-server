a184b76077380c55745913caf2c53503733ab1fb
Aryan-Khanijo <akhanijo12345@gmail.com>
2022-12-24 14:35:02 +0530

    user view fix

--- server/schema/migrations/20221224073922_basic_user_info_view.js
+++ server/schema/migrations/20221224073922_basic_user_info_view.js
@@ -5,7 +5,7 @@
 exports.up = function (knex) {
 	return knex.schema.createViewOrReplace('user_info_view', (view) => {
 		view.columns(['user_id', 'username', 'followers', 'following']);
-		view.as(knex.raw('select users.id, users.username, count(followers_view.followers), count(following_view.following), users.slug from users left join followers_view on users.id = followers_view.user_id left join following_view on users.id = following_view.user_id group by users.id;'))
+		view.as(knex.raw('select users.id, users.name, users.username, ARRAY_LENGTH(followers_view.followers, 1), ARRAY_LENGTH(following_view.following, 1), users.slug from users left join followers_view on users.id = followers_view.user_id left join following_view on users.id = following_view.user_id;'))
 	})
 };
 
